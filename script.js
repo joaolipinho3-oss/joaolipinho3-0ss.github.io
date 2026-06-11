db07450aef164def3195d3f030ce07fc1abd3ba1
joaolipinho3-oss <joaolipinho3@gmail.com>
2026-06-11 10:52:31 -0300

    modified:   index.html 	modified:   script.js 	modified:   styles.css 	Pasta CSS/

---
+++ script.js
@@ -0,0 +1,8 @@
+const buttonPlus = document.getElementById('soma') // Botão de +
+const buttonMinus = document.getElementById('subtracao') // Botão de menos
+
+const tela = document.getElementById('display-counter') // Tela do contador
+
+const increment = document.getElementById('changeBy') // Mudar incremento
+
+buttonMinus.addEventListener('click')
